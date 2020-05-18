fb4bc3de15791474487c36673b6919a0220b278e
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-05-17 22:34:10 -0400

    Loop Exercise

---
+++ Edabit/Repeat_the_Same.py
@@ -0,0 +1,24 @@
+#Repeat the Same Item Multiple Times
+#Create a function that takes two arguments (item, times). The first argument (item) is the item that needs repeating while the second argument (times) is the number of times the item is to be repeated. Return the result in a list.
+#item can be either a string or a number.
+#times will always be a number.
+def repeat(item,times):
+    l=[]
+    for i in range(0,times):
+        l.append(item)
+    return l
+
+
+
+print(repeat("edabit", 3) )
+#➞ ["edabit", "edabit", "edabit"]
+
+print(repeat(13, 5) )
+#➞ [13, 13, 13, 13, 13]
+
+print(repeat("7", 2) )
+#➞ ["7", "7"]
+
+print(repeat(0, 0) )
+#➞ []
+
