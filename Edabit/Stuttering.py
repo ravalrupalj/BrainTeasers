9451f981160488bb847149af02a13a12593184b8
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-12-20 22:51:29 -0500

    String_python

---
+++ Edabit/Stuttering.py
@@ -0,0 +1,3 @@
+def stutter(word):
+    return word[0]+word[1]+"... "+word[0]+word[1]+"... "+word+"?"
+print(stutter("incredible"))
---
+++ Edabit/convert.py
@@ -0,0 +1,9 @@
+
+
+def convert(a,b):
+    return (a*3600)+(b*60)
+
+
+
+print(convert(1, 3))
+#➞ 3780
